ab2c7921353c7d8c52e1923a99eff3f1a2233ac6
Wicky <1051807368@qq.com>
2018-09-13 17:30:28 +0800

    mod demo

--- draft/dom.v2.js
+++ draft/dom.v2.js
@@ -1,8 +1,50 @@
+document.getElementById('form1').innerHTML = '\
+	<div class="container">\
+	    <form class="form-horizontal">\
+	        <div class="form-group has-feedback">\
+	            <label for="username1" class="col-xs-3 control-label">Username 1：</label>\
+	            <div class="col-xs-9">\
+	                <input required type="text" class="form-control" name="username1" id="username1" />\
+	            </div>\
+	        </div>\
+	        <div class="form-group has-feedback">\
+	            <label for="password1" class="col-xs-3 control-label">Password 1：</label>\
+	            <div class="col-xs-9">\
+	                <input required type="password" class="form-control" name="password1" id="password1" />\
+	            </div>\
+	        </div>\
+	        <div class="form-group has-feedback">\
+	            <div class="col-xs-offset-3 col-xs-9">\
+	                <button type="submit" class="btn btn-info">Submit 1</button>\
+	            </div>\
+	        </div>\
+	    </form>\
+	</div>\
+';
+
+
 BS3.DOM.render(
-	BS3.DOM.createElement('div', {class: 'bg-success'}, [
-		BS3.DOM.createElement('p', {text: 'paragraph 1'}),
-		BS3.DOM.createElement('p', {text: 'paragraph 2', class: 'bg-danger'}),
-		BS3.Label.primary('OK').style('position: relative; left: 50%;')
+	BS3.DOM.createElement('div', {class: 'container'}, [
+		BS3.DOM.createElement('form', {class: 'form-horizontal'}, [
+			BS3.DOM.createElement('div', {class: 'form-group has-feedback'}, [
+				BS3.DOM.createElement('label', {text: 'Username 2：', attr: {for: 'username2'}, class: 'col-xs-3 control-label'}),
+				BS3.DOM.createElement('div', {class: 'col-xs-9'}, [
+					BS3.DOM.createElement('input', {attr: {required: 'required', type: 'text', name: 'username1', id: 'username2'}, class: 'form-control'})
+				])
+			]),
+			BS3.DOM.createElement('div', {class: 'form-group has-feedback'}, [
+				BS3.DOM.createElement('label', {text: 'Password 2：', attr: {for: 'password2'}, class: 'col-xs-3 control-label'}),
+				BS3.DOM.createElement('div', {class: 'col-xs-9'}, [
+					BS3.DOM.createElement('input', {attr: {required: 'required', type: 'text', name: 'password2', id: 'password2'}, class: 'form-control'})
+				])
+			]),
+			BS3.DOM.createElement('div', {class: 'form-group has-feedback'}, [
+				BS3.DOM.createElement('div', {class: 'col-xs-offset-3 col-xs-9'}, [
+					BS3.Button.info('Submit 2').attr({type: 'submit'})
+				])
+			])
+		])
 	]),
-	document.getElementById('dom')
+	document.getElementById('form2')
 );
+
